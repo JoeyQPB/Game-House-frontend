a2e146f1b59f170396c6b98d542f2c39346ab77b
joey <joey_qpb@hotmail.com>
2024-04-27 20:40:10 -0300

    fill game card

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -1,28 +1,82 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 
-function GameCard({ imageUrl, title, description }) {
+function GameCard({ imageUrl, title, description, gamePage }) {
+    const [isHovered, setIsHovered] = useState(false);
+
     return (
         <Row xs={1} md={2} className="g-4">
-            {
-                <Col >
-                    <Card style={{width: '35rem', height: '10.7rem'}}>
-                        <Row noGutters >
+            <Col>
+                <Link to={gamePage} style={{ textDecoration: 'none' }}>
+                    <Card
+                        style={{
+                            width: '35rem',
+                            height: '10.7rem',
+                            border: '6px solid #00008b',
+                            position: 'relative',
+                            overflow: 'hidden',
+                        }}
+                        onMouseEnter={() => setIsHovered(true)}
+                        onMouseLeave={() => setIsHovered(false)}
+                    >
+                        <Row noGutters>
                             <Col xs={4}>
-                                <Card.Img variant="top" src={imageUrl} />
+                                <Card.Img
+                                    variant="top"
+                                    src={imageUrl}
+                                    style={{ height: '10rem', width: '12rem', objectFit: 'cover' }}
+                                />
                             </Col>
                             <Col xs={8}>
                                 <Card.Body>
-                                    <Card.Title>{title}</Card.Title>
-                                    <Card.Text>{description}</Card.Text>
+                                    <Card.Title
+                                        style={{
+                                            fontSize: '20px',
+                                            fontWeight: 'bold',
+                                            color: '#333',
+                                            textAlign: 'center',
+                                            textTransform: 'uppercase',
+                                            marginBottom: '10px',
+                                        }}
+                                    >
+                                        {title}
+                                    </Card.Title>
+                                    <Card.Text
+                                        style={{
+                                            fontSize: '14px',
+                                            color: '#666',
+                                            textAlign: 'center',
+                                            marginBottom: '20px',
+                                        }}
+                                    >
+                                        {description}
+                                    </Card.Text>
+                                    {isHovered && (
+                                        <h1
+                                            style={{
+                                                position: 'absolute',
+                                                bottom: 0,
+                                                left: '50%',
+                                                transform: 'translateX(-50%)',
+                                                backgroundColor: 'rgba(0, 0, 0, 0.7)',
+                                                color: '#fff',
+                                                padding: '10px',
+                                                transition: '0.3s ease-in-out',
+                                            }}
+                                        >
+                                            Play Now
+                                        </h1>
+                                    )}
                                 </Card.Body>
                             </Col>
                         </Row>
                     </Card>
-                </Col>
-            }
+                </Link>
+            </Col>
         </Row>
     );
 }
@@ -31,6 +85,7 @@ GameCard.propTypes = {
     imageUrl: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
+    gamePage: PropTypes.string.isRequired
 };
 
 export default GameCard;
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -14,42 +14,61 @@ const Home = () => {
                     <GameCard
                         imageUrl="src/assets/joeykemon-pic.png"
                         title="JoeyKemon"
-                        description="JoeyKemon description"
+                        description="Explore diverse regions, capturing and training Pokémon.
+                         Engage in turn-based battles against other trainers and wild Pokémon to become the ultimate Pokémon Master."
+                        gamePage="https://joeyqpb.github.io/JoeyKemon-/"
                     />
                     <GameCard
                         imageUrl="src/assets/spaceInvaders-pic.png"
                         title="Space Invaders"
-                        description="Space Invaders Description"
+                        description="Defend Earth from an onslaught of descending alien invaders by controlling a laser cannon.
+                         Move horizontally to aim and shoot at the invaders while dodging their incoming fire.
+                         Destroy all aliens before they reach the bottom of the screen to progress to the next wave."
+                        gamePage="https://joeyqpb.github.io/Space-Invaders/"
                     />
                     <GameCard
                         imageUrl="src/assets/figthMaster-pic.png"
                         title="Figth Master"
-                        description="Figth Master Description"
+                        description="Engage in intense sword duels with a friend in this thrilling two-player game.
+                         Choose your character and clash swords in fast-paced battles."
+                        gamePage="https://joeyqpb.github.io/Fight-Master/"
                     />
                     <GameCard
                         imageUrl="src/assets/shootGame-pic.png"
                         title="Shoot Game"
-                        description="shootGame Description"
+                        description="Take control of a point at the center of the screen as balls approach from all sides. 
+                         Click to shoot and break incoming balls, testing your reflexes and aiming skills.
+                         Survive as long as possible to achieve the highest score."
+                        gamePage="https://joeyqpb.github.io/Shoot-Game/"
                     />
                     <GameCard
                         imageUrl="src/assets/KingJump-pic.png"
                         title="King Jump"
-                        description="KingJump Description"
+                        description="Take on the role of a majestic king on a quest for adventure.
+                         Navigate through the kingdom by leaping across the map, discovering hidden treasures, and overcoming obstacles.
+                         Explore the realm to uncover its secrets and establish your royal legacy."
+                        gamePage="https://joeyqpb.github.io/King-Jump/"
                     />
                     <GameCard
                         imageUrl="src/assets/hogart-pic.png"
                         title="hogart"
-                        description="hogart game Description"
+                        description="Embark on an epic journey through multiple levels as a fearless medieval warrior.
+                         Navigate treacherous obstacles, defeat enemies, and solve puzzles to reach the exit door and advance to the next level."
+                        gamePage="https://joeyqpb.github.io/Hogart-o-Terrivel-game/"
                     />
                     <GameCard
                         imageUrl="src/assets/littleSnake-pic.png"
                         title="littleSnake"
-                        description="littleSnake Description"
+                        description="Guide a growing snake to consume food while avoiding walls and its own tail.
+                         Keep the snake moving to score points and prevent it from crashing."
+                        gamePage="https://joeyqpb.github.io/little-snake/"
                     />
                     <GameCard
                         imageUrl="src/assets/pacMan-pic.webp"
                         title="Pac Man"
-                        description="Pac Man Description"
+                        description="Guide the iconic yellow character through a maze, gobbling up dots and fruit while evading colorful ghosts.
+                         Eat power pellets to turn the tables and chase down the ghosts. Clear each maze to advance to the next level."
+                        gamePage="building"
                     />
                 </div>
                 <div id="donate-section" className="donate-container">
