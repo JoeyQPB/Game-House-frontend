0772b80b97782595591bbd27d19e87847ce5e988
joey <joey_qpb@hotmail.com>
2024-05-08 15:15:13 -0300

    refact: donate componet

--- src/components/DonateComponent.jsx
+++ src/components/DonateComponent.jsx
@@ -1,20 +1,45 @@
 function DonateComponent() {
+
+    const divStyle = {
+        border: '1px solid #ccc',
+        padding: '20px',
+        backgroundColor: '#d3d3d3',
+        color: 'black',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center'
+    };
+
+    const containerStyle = {
+        display: 'grid',
+        gridTemplateColumns: 'repeat(3, 1fr)',
+        gridGap: '10px',
+        backgroundColor: '#565656',
+        color: '#000',
+        padding: '20px',
+    };
+
     return (
         <>
-            <div style={{ background: '#A9A9A9', width: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center', padding: '20px' }}>
-                <h3 style={{ fontWeight: 'bold', textAlign: 'center', marginBottom: '10px' }}>DONATE</h3>
-                <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                        <img src="src/assets/donate-pix.jpg" alt="QR Code" style={{ width: '16rem', marginBottom: '10px' }} />
-                        <p style={{ textAlign: 'center', fontSize: '10px' }}>
-                            00020126400014br.gov.bcb.pix0111009893535670203Pix5204000053039865802BR5925JOEY QUADROS PACHECO BAST6015VITORIA DA CONQ62240520DonatesFromGameHouse6304B8E7
-                        </p>
-                    </div>
-                    <div style={{ maxWidth: '400px', marginLeft: '20px' }}>
-                        <p style={{ textAlign: 'center' }}>
-                            Help support our software development efforts! Your generous donations enable us to continue creating valuable tools and resources for our community. Simply scan the QR code or copy the code below to contribute. I promise that all money collected goes towards development costs which include pizza, burgers and hardware for bitcoin mining. Thank you for your support!
-                        </p>
-                    </div>
+            <div style={containerStyle}>
+                <div style={divStyle}>
+                    <img src="src/assets/donate-pix.jpg" alt="QR Code" style={{ width: '16rem', marginBottom: '10px' }} />
+                    <p style={{ textAlign: 'center', fontSize: '10px', maxWidth: '35vw' }}>
+                        00020126400014br.gov.bcb.pix0111009893535670203Pix5204000053039865802BR5925
+                        JOEY QUADROS PACHECO BAST6015VITORIA DA CONQ62240520DonatesFromGameHouse6304B8E7
+                    </p>
+                </div>
+
+                <div style={{ ...divStyle, textAlign: 'center', fontSize: '1.2rem' }}>
+                    <p>
+                        Help support our software development efforts! Your generous donations enable us to continue creating valuable tools and resources for our community. Simply scan the QR code or copy the code below to contribute. I promise that all money collected goes towards development costs which include pizza, burgers and hardware for bitcoin mining.
+                    </p>
+                    <p>Thanks for support!</p>
+                </div>
+
+                <div style={divStyle}>
+                    <img src="src/assets/gatopidao.jpg" alt="gatinho" style={{ width: '100%', height: '100%', marginBottom: '10px' }} />
                 </div>
             </div>
         </>
