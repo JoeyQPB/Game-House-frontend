692ae2a4af594c7d8121d731068efe7bba3dd6a1
joey <joey_qpb@hotmail.com>
2024-05-08 15:15:00 -0300

    add: donate page

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,9 @@ import SuggestNewGame from './pages/SuggestNewGame.jsx'
 import SignInPage from './pages/SignInPage.jsx'
 import LoginPage from './pages/logIn.jsx'
 import ProfilePage from './pages/profile.jsx'
+import DonatePage from "./pages/Donate.jsx";
 
 function App() {
-
-
   return (
     <>
       <div className='app'>
@@ -19,6 +18,7 @@ function App() {
           <Route path='/' element={<Home />} />
           <Route path='/signIn' element={<SignInPage />} />
           <Route path='/login' element={<LoginPage />} />
+          <Route path='/donate' element={<DonatePage />} />
           <Route path='/profile' element={<ProfilePage />} />
           <Route path='/suggestNewGame' element={<SuggestNewGame />} />
         </Routes>
