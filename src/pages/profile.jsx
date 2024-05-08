7e5ab7ca2ea39071ff9bf8f4aca94c814eddd2af
joey <joey_qpb@hotmail.com>
2024-05-08 15:16:24 -0300

    sign out button

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,7 +1,21 @@
 import NavBarFrg from '../components/NavBarFrg'
 import ProfileComponent from '../components/profile/Profile.jsx'
+import Button from "react-bootstrap/Button";
+// import { useContext } from "react";
+// import { AuthContext } from "../../contexts/authContext";
+import { useNavigate } from "react-router-dom";
 
 const ProfilePage = () => {
+
+    // const { loggedInUser, setLoggedInUser } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    function handleSignOut() {
+        localStorage.removeItem("loggedInUser");
+        // setLoggedInUser(null);
+        navigate("/");
+    }
+
     return (
         <>
             <NavBarFrg />
@@ -14,6 +28,14 @@ const ProfilePage = () => {
             }}>
                 <h2 style={{ color: 'white' }}>Profile</h2>
                 <ProfileComponent />
+                <Button
+                    variant="danger"
+                    onClick={() => {
+                        handleSignOut();
+                    }}
+                >
+                    Sign out
+                </Button>
             </div>
         </>
     )
