342124b4cfb46d365f1453d28b1d88f44e356e9f
joey <joey_qpb@hotmail.com>
2024-06-29 15:10:32 -0300

    fix imgs

--- src/data/gamesData.jsx
+++ src/data/gamesData.jsx
@@ -1,48 +1,48 @@
 export const gamesData = [
     {
-        imageUrl: "assets/joeyKemon-pic.png",
+        imageUrl: "src/assetsData/joeyKemon-pic.png",
         title: "JoeyKemon",
         description: "Explore diverse regions, capturing and training Pokémon. Engage in turn-based battles against other trainers and wild Pokémon to become the ultimate Pokémon Master.",
         gamePage: "https://joeyqpb.github.io/JoeyKemon-/"
     },
     {
-        imageUrl: "assets/spaceInvaders-pic.png",
+        imageUrl: "src/assetsData/spaceInvaders-pic.png",
         title: "Space Invaders",
         description: "Defend Earth from an onslaught of descending alien invaders by controlling a laser cannon. Move horizontally to aim and shoot at the invaders while dodging their incoming fire.",
         gamePage: "https://joeyqpb.github.io/Space-Invaders/"
     },
     {
-        imageUrl: "../assets/figthMaster-pic.png",
+        imageUrl: "src/assetsData/figthMaster-pic.png",
         title: "Figth Master",
         description: "Engage in intense sword duels with a friend in this thrilling two-player game. Choose your character and clash swords in fast- paced battles.",
         gamePage: "https://joeyqpb.github.io/Fight-Master/"
     },
     {
-        imageUrl: "src/assets/shootGame-pic.png",
+        imageUrl: "src/assetsData/shootGame-pic.png",
         title: "Shoot Game",
         description: "Take control of a point at the center of the screen as balls approach from all sides. Click to shoot and break incoming balls, testing your reflexes and aiming skills. Survive as long as possible to achieve the highest score.",
         gamePage: "https://joeyqpb.github.io/Shoot-Game/"
     },
     {
-        imageUrl: "src/assets/KingJump-pic.png",
+        imageUrl: "src/assetsData/KingJump-pic.png",
         title: "King Jump",
         description: "Take on the role of a majestic king on a quest for adventure. Navigate through the kingdom by leaping across the map, discovering hidden treasures, and overcoming obstacles. Explore the realm to uncover its secrets and establish your royal legacy.",
         gamePage: "https://joeyqpb.github.io/King-Jump/"
     },
     {
-        imageUrl: "src/assets/hogart-pic.png",
+        imageUrl: "src/assetsData/hogart-pic.png",
         title: "Hogart",
         description: "Embark on an epic journey through multiple levels as a fearless medieval warrior. Navigate treacherous obstacles, defeat enemies, and solve puzzles to reach the exit door and advance to the next level.",
         gamePage: "https://joeyqpb.github.io/Hogart-o-Terrivel-game/"
     },
     {
-        imageUrl: "src/assets/littleSnake-pic.png",
+        imageUrl: "src/assetsData/littleSnake-pic.png",
         title: "littleSnake",
         description: "Guide a growing snake to consume food while avoiding walls and its own tail. Keep the snake moving to score points and prevent it from crashing.",
         gamePage: "https://joeyqpb.github.io/little-snake/"
     },
     {
-        imageUrl: "./src/assets/pacMan-pic.webp",
+        imageUrl: "src/assetsData/pacMan-pic.webp",
         title: "Pac Man",
         description: "Guide the iconic yellow character through a maze, gobbling up dots and fruit while evading colorful ghosts. Eat power pellets to turn the tables and chase down the ghosts.Clear each maze to advance to the next level.",
         gamePage: "https://joeyqpb.github.io/Pacman-Game/"
